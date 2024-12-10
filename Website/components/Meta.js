f3cce6860399abc2808b67142dfd5f86771c74c9
ykb0x01 <yohanopp@gmail.com>
2024-12-11 00:18:54 +0100

    hiiidd

--- Website/components/Meta.js
+++ Website/components/Meta.js
@@ -4,9 +4,12 @@ import Head from 'next/head'
 const Meta = ({ title, description }) => {
   return (
     <Head>
-        <meta name="viewport" content="height=device-height, width=device-width, minimum-scale=1.0, maximum-scale=1.0, initial-scale=1.0" />
-        <title>{title}</title>
-        <meta name='description' content={description} />
+      <meta name="viewport" content="height=device-height, width=device-width, minimum-scale=1.0, maximum-scale=1.0, initial-scale=1.0" />
+      <meta name="viewport" content="height=device-height, width=device-width, minimum-scale=1.0, maximum-scale=1.0, initial-scale=1.0" />
+      <meta name="viewport" content="height=device-height, width=device-width, minimum-scale=1.0, maximum-scale=1.0, initial-scale=1.0" />
+      <meta name="viewport" content="height=device-height, width=device-width, minimum-scale=1.0, maximum-scale=1.0, initial-scale=1.0" />
+      <title>{title}</title>
+      <meta name='description' content={description} />
     </Head>
   )
 }
